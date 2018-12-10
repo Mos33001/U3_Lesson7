7063d1cf580202a32e79f0083027c5be621dfc55
Mos33001 <43756312+Mos33001@users.noreply.github.com>
2018-12-10 12:39:13 -0500

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,4 @@
+x = input("Pick a number:")
+y = input("Pick another number:")
+product = int(x) * int(y)
+print(product)
---
+++ p3.py
@@ -0,0 +1,9 @@
+print("-" * 80)
+
+x = input("What is your age: ")
+x = int(x)
+
+total = 100 - x
+year = 2018 + total
+
+print("You will turn 100 in the year " + str(year))
